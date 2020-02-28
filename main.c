54d5c515656602b4cfc342c42cba8cc2bc0c2484
Gamra <ilyassstudies2017@gmail.com>
2020-02-28 06:31:31 +0100

    feat(fo): everything that the scenario needs

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ void choiceTheme(int theme)
     }
     else if (theme == 2)
     {
-        //themeTheCatNeighbors();
+        TrevorTalent();
     }
 }
 
@@ -83,7 +83,7 @@ void initial(int *theme)
     printf("Répondez au questions et survivez jusqu'a la fin du jeu\n");
     printf("                   Bonne chance !                      \n");
     printf("-------------------------------------------------------\n\n");
-    askQuestion("Quelle thème voulez vous ?", "Rencard avec noémie", "Le chat des voisins");
+    askQuestion("Quelle thème voulez vous ?", "Rencard avec noémie", "TrevorDream(English)");
     scanf("%i", theme);
     checkInput(*theme);
 }
@@ -127,7 +127,31 @@ void themeDateWithNoemie()
     checkAnswer(answer[2], goodAnswer[2], "QUand elle est arriver au macdo elle vous a demander quand arriver son menu maxi best of \n, vous etes passer pour un minable et vous etes enfuis en courrant bouuuuuuuu");
     // end question 3
 }
-void NeighborsCat()
+void TrevorTalent()
 {
-  char name[] = "Le chat des voisins"; 
+  char name[] = "Football talent";
+  int answer[7] = {0};
+  int goodAnswer[7] = {1, 2, 2, 1, 2, 2, 1};
+  loadTheme(name);
+  // First question
+  askQuestion("Trevor is a highschool student, but not like any other student, he got a godgiven talent in soccer not has ever \nwitnessed something like it before, at first he enjoyed playing football more than anything else when he was a kid, but when he reached highschool his parents and everyone close to him started obligating him to be a very good football player \nfor the sake a scholarship to a good university to the point where he couldn't enjoy his teen years, one day he was helping his parents move some old stuff from the attic, and when he was alone he got the idea to\n",
+              "jump off the attic without ladder",
+              "Ne pas engager la conversation");
+  scanf("%i", &answer[0]);
+  checkInput(answer[0]);
+  checkAnswer(answer[0], goodAnswer[0], "Trevor is having depression");
+  // end first questions
+  // Second Question
+  askQuestion("Trevor did not jump and he surivied \nto live another day playing what he loves \nis he gonna play football for the sake of his love to the game \n",
+              "play because he love the game ",
+              "to get what his parents wants");
+  scanf("%i", &answer[1]);
+  checkInput(answer[1]);
+  checkAnswer(answer[1], goodAnswer[1], "trevor start working hard to reach his dream goals which is join a profesional Team");
+  askQuestion("while Trevor was working hard to reach his dream\n his parents annoying him to give up on his dreams and start working on real goals\n because they dont belive in something called football \nthey care only about his scholarship",
+              "follow his parents path",
+              "keep pushing to reach his dreams");
+  scanf("%i", &answer[2]);
+  checkInput(answer[2]);
+  checkAnswer(answer[2], goodAnswer[2], "Trevor made it to the top and became a Football\n star this is a happy ending so wake up and work your ass off to be like Trevor");
 }
